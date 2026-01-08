1e0ac578bbaa6d8818efad6447f45a3d7348bd20
Sumit Dhoundiyal <your-sumit.dhoundiyal.56@gmail.com>
2026-01-08 15:30:15 +0530

    yes

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return " hi Sumit dhoundiyal your code is working fine,you are now ready to give demo to pavan and amit and demonstrate what you have done so far"
+    return " sun bhosdikey"
